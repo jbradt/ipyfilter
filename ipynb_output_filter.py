df88f92213e521f2ac5a2a3c691d60a1e3b9b9ca
Joshua Bradt <bradt@nscl.msu.edu>
2015-11-09 09:34:10 -0500

    Changed filter for new Jupyter package names.

--- ipynb_output_filter.py
+++ ipynb_output_filter.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
 import sys
-from IPython.nbformat import read, write, NO_CONVERT
+from nbformat import read, write, NO_CONVERT
 
 nb = read(sys.stdin, NO_CONVERT)
 
 for cell in nb.cells:
-	if cell.cell_type == 'code':
-		cell.outputs = []
-		cell.execution_count = None
+    if cell.cell_type == 'code':
+        cell.outputs = []
+        cell.execution_count = None
 
 write(nb, sys.stdout, NO_CONVERT)
